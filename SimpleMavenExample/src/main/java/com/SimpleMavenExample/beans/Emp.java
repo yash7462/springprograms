57bd225ee469c8aaf51d206efdc0b2b8c1af35f0
yash7462 <yashpatel7462@gmail.com>
2020-02-14 14:40:50 +0530

    first commit SimpleMavenExample

---
+++ SimpleMavenExample/src/main/java/com/SimpleMavenExample/beans/Emp.java
@@ -0,0 +1,42 @@
+package com.SimpleMavenExample.beans;
+
+public class Emp {
+
+	private int id;
+	private String name;
+	private double salary;
+	private String designation;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
